c23b44204dd70ec67cd8095fc14d5dd645d6313b
yonatanharel <Interlaken23>
2023-09-11 10:15:34 +0300

    Push existing project to GitLab

---
+++ monthly_challenges/challenges/views.py
@@ -0,0 +1,46 @@
+from django.http import (HttpResponse, HttpResponseNotFound,
+                         HttpResponseRedirect)
+from django.shortcuts import render
+from django.template.loader import render_to_string
+from django.urls import reverse
+
+monthly_challenges = {
+    "january":"eat no meat for the entire month!",
+    "february": "Walk for at least 20 minutes every day!",
+    "march": "Learn Django at least 20 minutes every day!",
+    "april":"eat no meat for the entire month!",
+    "may": "Walk for at least 20 minutes every day!",
+    "june": "Learn Django at least 20 minutes every day!",
+    "july":"eat no meat for the entire month!",
+    "august": "Walk for at least 20 minutes every day!",
+    "september": "Learn Django at least 20 minutes every day!",
+    "october":"eat no meat for the entire month!",
+    "november": "Walk for at least 20 minutes every day!",
+    "december": None
+}
+
+def index(request):
+    months = list(monthly_challenges.keys())
+    return render(request, "challenges/index.html", {
+        "months": months
+    })
+
+def monthly_challenge_by_number(request, month):
+    months = list(monthly_challenges.keys())
+    try:
+        redirect_month = months[month-1]
+    except:
+        return HttpResponseNotFound("<h1>Invalid month!")
+    redirect_path = reverse("month-challenge", args=[redirect_month])
+    return HttpResponseRedirect(redirect_path)
+
+def monthly_challenge(request, month):
+    try:
+        challenge_text = monthly_challenges[month]
+        return render(request, "challenges/challenge.html", {
+            "month_name": month,
+            "text": challenge_text
+        })
+    except:
+        return HttpResponseNotFound("<h1>This month is not supported!</h1>")
+    
